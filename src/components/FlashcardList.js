9c54f5cf050cdfe4f42ad69e3d592d7cedb2dbdf
eric santos <eric355402@yahoo.com>
2020-11-01 14:45:17 -0700

    tied Data to state

--- src/components/FlashcardList.js
+++ src/components/FlashcardList.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 import Flashcard from "./Flashcard";
 import Data from "../Data.json";
 
 // map over flashcard component, import json data here
 
 const FlashcardList = () => {
+  const [cards, setCards] = useState({ Data });
   return (
     <div>
-      {Data.map((post) => {
+      {cards.Data.map((post) => {
         return (
           <Flashcard
             key={post.id}
